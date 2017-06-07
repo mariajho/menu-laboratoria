2b2fa9f2ed3818377f3358abe48be9e7605daea0
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-06-06 23:10:51 -0400

    falta-menu-desplegable

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+/*var recuadro = document.getElementById("recuadro");
+recuadro.addEventListener("click",function(){
+    document.getElementById("menu2").classList.toggle("menu-desplegable");
+
+});*/
